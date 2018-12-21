665c884b6e74ef065e0b8bb49ec46409364b2389
Kilerd Chan <blove694@gmail.com>
2018-12-21 17:21:59 +0800

    flipping-an-image

---
+++ flipping-an-image/src/main.rs
@@ -0,0 +1,44 @@
+struct Solution;
+
+impl Solution {
+    pub fn flip_and_invert_image(a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        a.into_iter()
+            .map(|mut v| {
+                v.reverse();
+                v.into_iter()
+                    .map(|element| element ^ 1)
+                    .collect::<Vec<i32>>()
+            })
+            .collect()
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test1() {
+    assert_eq!(
+        Solution::flip_and_invert_image(vec![vec![1, 1, 0], vec![1, 0, 1], vec![0, 0, 0]]),
+        vec![vec![1, 0, 0], vec![0, 1, 0], vec![1, 1, 1]]
+    )
+}
+
+#[test]
+fn test2() {
+    assert_eq!(
+        Solution::flip_and_invert_image(vec![
+            vec![1, 1, 0, 0],
+            vec![1, 0, 0, 1],
+            vec![0, 1, 1, 1],
+            vec![1, 0, 1, 0]
+        ]),
+        vec![
+            vec![1, 1, 0, 0],
+            vec![0, 1, 1, 0],
+            vec![0, 0, 0, 1],
+            vec![1, 0, 1, 0]
+        ]
+    )
+}
